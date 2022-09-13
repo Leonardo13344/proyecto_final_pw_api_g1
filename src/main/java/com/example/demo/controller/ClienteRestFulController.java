d80837c22e874da2ec4a6c4ca6c1630ef48b2987
Leonardo13344 <lrsuniga@uce.edu.ec>
2022-09-13 18:05:25 -0500

    update

--- src/main/java/com/example/demo/controller/ClienteRestFulController.java
+++ src/main/java/com/example/demo/controller/ClienteRestFulController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -54,8 +54,13 @@ public class ClienteRestFulController {
 
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<ClienteTo> findByCedula(@RequestParam(value = "cedula") String cedula) {
-		ClienteTo aux = this.clienteService.findByCedulaTo(cedula);
-		return ResponseEntity.ok(aux);
+		try {
+			ClienteTo aux = this.clienteService.findByCedulaTo(cedula);
+			return ResponseEntity.ok(aux);
+		} catch (Exception e) {
+			// TODO: handle exception
+			return null;
+		}
 	}
 	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/vips")
